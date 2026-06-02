8fb8816fe92b8502323927a1056b736c8b9089f1
규 서문 <illsi@naver.com>
2026-06-02 22:31:42 +0900

    ICombatabt interface

--- Source/APGame/Interface/Combatabt.h
+++ Source/APGame/Interface/Combatabt.h
@@ -14,6 +14,21 @@ class UCombatabt : public UInterface
 };
 
 /**
+ * Combat state enum and interface for actors that can perform attacks/guards.
+ */
+UENUM(BlueprintType)
+enum class ECombatState : uint8
+{
+	Idle            UMETA(DisplayName = "Idle"),
+	WeakAttacking   UMETA(DisplayName = "Weak Attacking"),
+	StrongAttacking UMETA(DisplayName = "Strong Attacking"),
+	Guarding        UMETA(DisplayName = "Guarding"),
+	Dodging         UMETA(DisplayName = "Dodging"),
+	Stunned         UMETA(DisplayName = "Stunned"),
+	Max             UMETA(Hidden)
+};
+
+/**
  * 
  */
 class APGAME_API ICombatabt
@@ -22,8 +37,15 @@ class APGAME_API ICombatabt
 
 	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
 public:
+	// State-based accessors (preferred for extensibility)
+	virtual ECombatState GetCombatState() const = 0;
+	virtual void SetCombatState(ECombatState NewState) = 0;
+
+	// Attacks
 	virtual void WeakAttack() = 0;
 	virtual void StrongAttack() = 0;
-	virtual bool IsGuarded() = 0;
-	virtual void Guard() = 0;
+
+	// Convenience helpers that use the state accessors. Implementing classes may override if needed.
+	virtual bool IsGuarded() const { return GetCombatState() == ECombatState::Guarding; }
+	virtual void Guard() { SetCombatState(ECombatState::Guarding); }
 };
